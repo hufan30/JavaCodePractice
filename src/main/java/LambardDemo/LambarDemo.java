6eb861d078c5042fb18b8e498e24b51366c347dd
hufan <hufan30@163.com>
2019-12-21 14:36:03 +0800

    update

---
+++ src/main/java/LambardDemo/LambarDemo.java
@@ -0,0 +1,26 @@
+package LambardDemo;
+
+/**
+ * @description:
+ * @author: HuFan
+ * @time: 2019/12/209:39 下午
+ **/
+public class LambarDemo {
+    
+    public static class PretendEat{
+        static void Peat(){
+            System.out.println("假装我是eat");
+        }
+    }
+
+    void testEat(ActionEat ae){
+        ae.eat();
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        new LambarDemo().testEat(PretendEat::Peat);
+    }
+}
+interface ActionEat{
+    void eat();
+}
