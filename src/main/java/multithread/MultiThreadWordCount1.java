9cc5e0f792bea398c677b236e59de4912beb39aa
hufan <hufan30@163.com>
2020-02-03 22:49:52 +0800

    update

---
+++ src/main/java/multithread/MultiThreadWordCount1.java
@@ -0,0 +1,40 @@
+package multithread;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MultiThreadWordCount1 {
+    // 使用threadNum个线程，并发统计文件中各单词的数量
+    public static Map<String, Integer> count(int threadNum, List<File> files) throws IOException, InterruptedException {
+        Map<String, Integer> result = new ConcurrentHashMap<>();
+        AtomicInteger restFileNumber = new AtomicInteger(files.size());
+        for (File file : files) {
+            new Thread(() -> {
+                try {
+                    Map<String, Integer> countResult = Utils.countFile(file);
+                    synchronized (result) {
+                        //这里使用merge到第一个result中，是因为runable没有返回值；
+                        Utils.mergeIntoFirstMap(result, countResult);
+                        if (restFileNumber.decrementAndGet() == 0) {
+                            result.notify();
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+
+        synchronized (result) {
+            while (restFileNumber.intValue() > 0) {
+                result.wait();
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/multithread/ProducerConsumer3.java
@@ -0,0 +1,52 @@
+package multithread;
+
+import java.util.Random;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class ProducerConsumer3 {
+    static BlockingDeque que = new LinkedBlockingDeque(1);
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
+
+    public static class Producer extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i <= 10; i++) {
+                int num = new Random().nextInt();
+                System.out.println("Producing " + num);
+                try {
+                    que.put(num);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static class Consumer extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i <= 10; i++) {
+                Integer poll = null;
+                try {
+                    poll = (Integer) que.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Consuming " + poll);
+            }
+        }
+    }
+}
