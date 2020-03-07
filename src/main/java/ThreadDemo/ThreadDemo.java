b5142a59b0f302b9e10bfca7c265a1f635d7b4c3
hufan <hufan30@163.com>
2020-03-07 15:09:16 +0800

    update

--- src/main/java/ThreadDemo/ThreadDemo.java
+++ src/main/java/ThreadDemo/ThreadDemo.java
@@ -7,4 +7,21 @@ package ThreadDemo;
  **/
 public class ThreadDemo {
     Runnable a = () -> System.out.println(123);
+
+    //线程中创建的对象，是每个线程私有的；
+    //但是这个a,是公有的；
+    public static void main(String[] args) {
+        Apple a = new Apple();
+
+        for(int i=0;i<4;i++){
+            new Thread(()->{
+                System.out.println(new Apple());
+                System.out.println(Thread.currentThread());
+                System.out.println(a);
+            }).start();
+        }
+    }
+}
+class Apple{
+
 }
