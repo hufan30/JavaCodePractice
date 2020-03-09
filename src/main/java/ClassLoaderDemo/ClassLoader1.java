8c99f78675d90a235bf694c09a35152929069abe
hufan <hufan30@163.com>
2020-03-09 23:36:43 +0800

    update

---
+++ src/main/java/ClassLoaderDemo/ClassLoader1.java
@@ -0,0 +1,15 @@
+package ClassLoaderDemo;
+
+import stream.Problem1;
+
+/**
+ * @description:
+ * @author: HuFan
+ * @time: 2020/3/911:00 下午
+ **/
+public class ClassLoader1 {
+    public static void main(String[] args) {
+        new Problem1();
+        System.out.println(1);
+    }
+}
