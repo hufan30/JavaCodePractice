cd8a0ab326fbd4bbb7b29b93537863b140e2f8fc
hufan <hufan30@163.com>
2020-03-16 21:46:24 +0800

    update

--- src/main/java/ClassLoaderDemo/ClassLoader1.java
+++ src/main/java/ClassLoaderDemo/ClassLoader1.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * @description:
@@ -13,12 +15,17 @@ import java.nio.file.Files;
  * @time: 2020/3/911:00 下午
  **/
 public class ClassLoader1 {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, SQLException {
         Class c1 = Problem1.class;
         Class c2 = new myClassLoader().loadClass("stream.Problem1");
         Object o = c2.getConstructor().newInstance();
-        Problem1 p1 = (Problem1)o; //这里会报无法cast的异常，这是因为类名+classloader才是唯一确定一个实例的根据；
+//        Problem1 p1 = (Problem1)o; //这里会报无法cast的异常，这是因为类名+classloader才是唯一确定一个实例的根据；
         System.out.println();
+
+        ClassLoader c3 = ClassLoader1.class.getClassLoader();
+        c3.loadClass("java.util.stream.Stream");
+
+        DriverManager.getConnection("");
     }
 }
 
