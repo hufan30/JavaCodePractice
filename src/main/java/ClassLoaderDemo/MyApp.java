653956635793e7d6fc5c339f0195393f3273897d
hufan <hufan30@163.com>
2020-03-19 22:21:26 +0800

    update

---
+++ src/main/java/ClassLoaderDemo/MyApp.java
@@ -0,0 +1,15 @@
+package ClassLoaderDemo;
+
+/**
+ * @description:
+ * @author: HuFan
+ * @time: 2020/3/178:11 下午
+ **/
+public class MyApp {
+    public static void main(String[] args) throws InterruptedException {
+        while(true){
+            Thread.sleep(1000);
+            System.out.println("123");
+        }
+    }
+}
