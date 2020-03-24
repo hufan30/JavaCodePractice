b2e2d6d6f4a953cdf2cb9e8505583ac7a534595d
hufan <hufan30@163.com>
2020-03-24 23:30:55 +0800

    update

---
+++ src/main/java/interfaceDemo/B.java
@@ -0,0 +1,13 @@
+package interfaceDemo;
+
+/**
+ * @description:
+ * @author: HuFan
+ * @time: 2020/3/244:27 下午
+ **/
+public class B extends A {
+    public static void main(String[] args) {
+        B b = new B();
+        b.foo();
+    }
+}
