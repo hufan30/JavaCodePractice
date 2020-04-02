311720fcba5220ea305369f6a54272f07bd7b84f
hufan30 <hufan30@163.com>
2020-04-02 08:48:22 +0800

    Update CustomWebMvcConfig.java

--- src/main/java/SpringMVCBootDemo/Controller/CustomWebMvcConfig.java
+++ src/main/java/SpringMVCBootDemo/Controller/CustomWebMvcConfig.java
@@ -1,12 +1,9 @@
 package SpringMVCBootDemo.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
-import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 
 import java.util.List;
 
