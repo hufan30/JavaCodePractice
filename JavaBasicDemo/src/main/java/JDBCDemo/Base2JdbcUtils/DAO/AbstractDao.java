74050c3b7bb65ea76f150caaaa10c3b89bcc5be7
hufan30 <hufan30@163.com>
2020-05-28 10:29:20 +0800

    update

--- JavaBasicDemo/src/main/java/JDBCDemo/Base2JdbcUtils/DAO/AbstractDao.java
+++ JavaBasicDemo/src/main/java/JDBCDemo/Base2JdbcUtils/DAO/AbstractDao.java
@@ -8,7 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class AbstractDao {
+public abstract class AbstractDao {
     // 增删改
     public int update(String sql, Object...args) {
         Connection conn = null;
@@ -29,4 +29,38 @@ public class AbstractDao {
             JDBCUtils.free(rs, ps, conn);
         }
     }
+
+    //查询
+    public Object query(String sql, Object[] args) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            // sql由调用者传入
+            ps = conn.prepareStatement(sql);
+            // 遍历设置模板参数
+            for (int i = 0; i < args.length; i++)
+                ps.setObject(i + 1, args[i]);
+            rs = ps.executeQuery();
+            Object obj = null;
+            // 结果集映射，子类必须实现抽象方法rowMapper
+            if (rs.next()) {
+                obj = rowMapper(rs);
+            }
+            return obj;
+        } catch (SQLException e) {
+            throw new DaoException(e.getMessage(), e);
+        } finally {
+            JDBCUtils.free(rs, ps, conn);
+        }
+    }
+
+    // 定义成抽象方法，让子类去实现
+    /**
+     * 这里也有源码的味道；
+     * 某些抽象方法，父类无法制定一个通用代码满足所有子类的结果集映射，
+     * 因为只有子类自己知道映射规则。所以，我们只能把结果集映射的权利交还给子类去实现。
+     */
+    abstract protected Object rowMapper(ResultSet rs);
 }
--- JavaBasicDemo/src/main/java/JDBCDemo/Base2JdbcUtils/DAO/UserDAOImlAbstract.java
+++ JavaBasicDemo/src/main/java/JDBCDemo/Base2JdbcUtils/DAO/UserDAOImlAbstract.java
@@ -5,6 +5,8 @@ import JDBCDemo.Base2JdbcUtils.POJO.User;
 /**
  * 从这里开始，使用了模板方法，抽取了AbstractDao之后，明显感觉到有源代码的味道了；
  * 而且从设计和复用的合理性来说，也是大大提升；
+ * AbstractDao的作用仅仅是“组装sql语句并执行”，不涉及差异性代码。
+ * UserDao/StudentDao/TeacherDao...等有差异的代码仅仅是sql和参数，交给具体实现类编写。
  */
 public class UserDAOImlAbstract extends AbstractDao implements UserDao {
     //增
