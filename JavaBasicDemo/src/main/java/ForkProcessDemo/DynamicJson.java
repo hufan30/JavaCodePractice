cb1b59bd6b5e077356946b7411e616c9f7e0e023
hufan <hufan30@163.com>
2020-06-27 18:10:54 +0800

    update

--- JavaBasicDemo/src/main/java/ForkProcessDemo/DynamicJson.java
+++ JavaBasicDemo/src/main/java/ForkProcessDemo/DynamicJson.java
@@ -1,9 +1,8 @@
 package ForkProcessDemo;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import org.apache.tomcat.jni.File;
+
+import java.io.*;
 
 /**
  * @description:
@@ -19,16 +18,29 @@ public class DynamicJson {
         //使用io,buffer,reader的传统方式
         Process process = pb.start();
         InputStream inputStream = process.getInputStream();
+        //此时测试fileinputStream
+        InputStream inputStream1 = new FileInputStream("./pom.xml");
         //此时拿到InputStream,是命令行执行的返回stream，下一步是bufferreader
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream1));
+        BufferedReader reader = new BufferedReader(new FileReader("pom.xml"));
+
         StringBuffer sb = new StringBuffer();
         String line = null;
         //下面应该是常用模式，reader的基本模版
-        while ((line = reader.readLine()) != null){
+        while ((line = reader.readLine()) != null) {
             sb.append(line).append("\n");
         }
         System.out.println(sb.toString());
 
-
+        /**
+         * 下面演示filereader，确实相比于buffered要低效率很多
+         */
+        FileReader reader1 = new FileReader("pom.xml");
+        char[] a = new char[50];
+        reader1.read(a);
+        for(char c:a){
+            System.out.print(c);
+        }
+        reader1.close();
     }
 }
