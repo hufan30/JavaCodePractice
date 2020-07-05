5de645ccc69d832cd0665401a5be4008d1811df0
hufan <hufan30@163.com>
2020-07-05 11:02:30 +0800

    update

---
+++ JavaBasicDemo/src/main/java/RedisDemo/DistributedLockTest.java
@@ -0,0 +1,87 @@
+package RedisDemo;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.UUID;
+
+/**
+ * @author hufan
+ * @date 2020/7/4 9:00 下午
+ * @annotation
+ */
+public class DistributedLockTest {
+    public static void main(String[] args) throws InterruptedException {
+        //10个客户端
+        //get key
+        //+1
+        //set key
+        //这里默认的jedisPool,应该是默认本地6379端口；
+        JedisPool jedisPool = new JedisPool();
+        DistributedLock distributedLock = new DistributedLock();
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+
+                try (Jedis jedis = jedisPool.getResource();) {
+                    String uuid = distributedLock.lock(jedis);
+                    String key = jedis.get("key");
+                    if (key == null) {
+                        key = "1";
+                    } else {
+                        key = "" + (Integer.parseInt(key) + 1);
+                    }
+                    jedis.set("key", key);
+                    distributedLock.unlock(jedis,uuid);
+                }
+            }).start();
+        }
+        Thread.sleep(5000);
+    }
+}
+
+class DistributedLock {
+
+    public DistributedLock() {
+    }
+
+    /**
+     * 分布式锁，这里先向里面set if not exist;
+     * 若是锁被别人拿了就一直循环，联系到乐观锁的概念；
+     * @param jedis
+     * @return
+     */
+    String lock(Jedis jedis) {
+        String uuid = UUID.randomUUID().toString();
+
+        try {
+            while (true) {
+                Long num = jedis.setnx("lock", uuid);
+                if (num == 1) {
+                    System.out.println("LOCK"+uuid);
+                    return uuid;
+                }
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    void unlock(Jedis jedis,String uuid) {
+        try {
+            while(true) {
+                String num = jedis.get("lock");
+                if (num.equals(uuid)) {
+                    System.out.println("Unlock!");
+                    jedis.del("lock");
+                    return;
+                }
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {
+           throw new RuntimeException(e);
+        }
+    }
+}
+
