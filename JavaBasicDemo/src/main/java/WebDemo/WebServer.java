1683bde4197fa373bfffa13cc0cf546a534638c4
hufan <hufan30@163.com>
2020-07-19 23:23:05 +0800

    Create WebServer.java

---
+++ JavaBasicDemo/src/main/java/WebDemo/WebServer.java
@@ -0,0 +1,25 @@
+package WebDemo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author hufan
+ * @date 2020/7/19 10:19 下午
+ * @annotation
+ */
+public class WebServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket();
+        serverSocket.bind(new InetSocketAddress("localhost", 8080));
+
+        Socket socket = serverSocket.accept();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String line = reader.readLine();
+        System.out.println(line);
+    }
+}
