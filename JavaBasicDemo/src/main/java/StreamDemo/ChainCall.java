bc1a2193fc2cc52105cf46ad427a8ea467c2784c
hufan30 <hufan30@163.com>
2020-10-15 17:24:21 +0800

    update

---
+++ JavaBasicDemo/src/main/java/StreamDemo/ChainCall.java
@@ -0,0 +1,42 @@
+package StreamDemo;
+
+public class ChainCall {
+    public static void main(String[] args) {
+//        Person bravo1988 = new Person("hufan",1).setName("bravo1988").setAge(18).setMoney(1000.0);
+    }
+}
+
+//class Person{
+//    private String name;
+//    private Integer age;
+//    private Double money;
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public Integer getAge() {
+//        return age;
+//    }
+//
+//    public Double getMoney() {
+//        return money;
+//
+//    }
+//
+//    public Person setName(String name) {
+//        this.name = name;
+//        // set方法要返回this，方便继续调用下一个set
+//        return this;
+//    }
+//
+//    public Person setAge(Integer age) {
+//        this.age = age;
+//        return this;
+//    }
+//
+//    public Person setMoney(Double money) {
+//        this.money = money;
+//        return this;
+//    }
+//}
