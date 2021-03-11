6997f4b49a39258f1e7ef6952732dcb58bfd6458
hufan30 <hufan30@163.com>
2021-03-11 15:12:21 +0800

    update

--- SpringBootDemo/src/main/java/com/MainDemo.java
+++ SpringBootDemo/src/main/java/com/MainDemo.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class MainDemo {
 
+    /**
+     * 这个main方法是废的，需要在com下面几层建立main才生效
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(MainDemo.class, args);
     }
