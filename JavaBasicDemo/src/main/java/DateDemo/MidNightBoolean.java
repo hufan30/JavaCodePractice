71fabd2ebd89a6c467c2ff03851c546876f8327d
hufan <hufan30@163.com>
2023-07-21 20:58:34 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	JavaBasicDemo/src/main/java/RedisDemo/DistributedLockTest.java

---
+++ JavaBasicDemo/src/main/java/DateDemo/MidNightBoolean.java
@@ -0,0 +1,29 @@
+package DateDemo;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * @author hufan
+ * @date 2023/7/21 20:30
+ * @annotation
+ */
+public class MidNightBoolean {
+
+    public static boolean getBooleanByDay() {
+        LocalDateTime oldDay = LocalDateTime.of(2023, 7, 1, 0, 0, 0);
+        Duration between = Duration.between(oldDay, LocalDateTime.now());
+        long l = between.toDays();
+        return (l & 1) == 1;
+    }
+
+
+    public static void main(String[] args) {
+        boolean booleanByDay = getBooleanByDay();
+        System.out.println(booleanByDay);
+
+    }
+}
--- JavaBasicDemo/src/main/java/DesignPatternDemo/InitDemo.java
+++ JavaBasicDemo/src/main/java/DesignPatternDemo/InitDemo.java
@@ -1,5 +1,9 @@
 package DesignPatternDemo;
 
+import com.beust.jcommander.internal.Lists;
+
+import java.util.List;
+
 /**
  * @description: 饿汉式的简单实例，大部分生产中该方式即可满足需求
  * @author: HuFan
@@ -9,6 +13,9 @@ public class InitDemo {
     public static InitDemo initDemo = new InitDemo();
 
     private InitDemo() {
+        List<Object> objects = Lists.newArrayList();
+        objects.clear();
+
     }
 
     public static class InnerTest{} //静态内部类，才能在main方法里new
